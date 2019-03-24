1e55277fb9ef1289119f937c17238223e0b6533f
zhouwumi <1454680068@qq.com>
2019-03-24 22:25:02 +0800

    一个简单的ugui基本使用示例

---
+++ UGUITest/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClick()
+	{
+		print ("我被点击了");	
+	}
+
+	public void OnValueChange(bool isSelected)
+	{
+		print ("Troggle Value is Changed   " + isSelected);
+	}
+}
